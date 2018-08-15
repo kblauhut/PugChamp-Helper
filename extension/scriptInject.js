83254919d41048aef0f2ef226275f57da95593d7
unknown <kodeeey@gmail.com>
2018-08-15 13:37:08 +0200

    -Added content script -Updated file location

---
+++ extension/scriptInject.js
@@ -0,0 +1,9 @@
+var colorPrem = "#FFEA32";
+var colorD1 = "#EB4B4B";
+var colorD2 = "#D32CE6";
+var colorMid = "#8847FF";
+var colorLow = "#4B69FF";
+var colorOpen = "#5E98D9";
+var colorNoDiv = "#B0C3D9";
+
+console.log("test");
