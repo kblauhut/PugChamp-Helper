2bf4946f0f58b092bcfca686aa3f78d030c414d2
sivbraec <simon.vanbraeckel@ugent.be>
2018-08-15 13:09:59 +0000

    Change popup active domain to eu.pug.champ.gg

--- background.js
+++ background.js
@@ -7,11 +7,9 @@ chrome.runtime.onInstalled.addListener(function() {
 
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'developer.chrome.com'},
-      })
-      ],
-          actions: [new chrome.declarativeContent.ShowPageAction()]
+        conditions: [new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: {hostEquals: 'eu.pug.champ.gg'}})],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
     }]);
   });
 });
