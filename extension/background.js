46b91437944dd6d6e450661d40438ee5ce37cd7e
unknown <kodeeey@gmail.com>
2018-08-15 16:52:31 +0200

    -Merging both extensions

--- extension/background.js
+++ extension/background.js
@@ -1,3 +1,16 @@
 chrome.runtime.onInstalled.addListener(function() {
+  console.log("onInstalled function called.");
   // TODO: Download player data from server.
+  
+  chrome.storage.sync.set({colorCoding: true, nameSubstitution: true}, function() {
+      console.log("variables 'colorCoding' and nameSubstitution set to true. Should be accessible from anywhere in the extension now.");
+    });
+
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+        conditions: [new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: {hostEquals: 'eu.pug.champ.gg'}})],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+  });
 });
