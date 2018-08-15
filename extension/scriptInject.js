e400d57753e8ba0488f03f4feac33fe1448f24b3
unknown <kodeeey@gmail.com>
2018-08-15 16:20:30 +0200

    -Added event listener

--- extension/scriptInject.js
+++ extension/scriptInject.js
@@ -1,9 +1,26 @@
-var colorPrem = "#FFEA32";
-var colorD1 = "#EB4B4B";
-var colorD2 = "#D32CE6";
-var colorMid = "#8847FF";
-var colorLow = "#4B69FF";
-var colorOpen = "#5E98D9";
-var colorNoDiv = "#B0C3D9";
+const colorPrem = "#FFEA32";
+const colorD1 = "#EB4B4B";
+const colorD2 = "#D32CE6";
+const colorMid = "#8847FF";
+const colorLow = "#4B69FF";
+const colorOpen = "#5E98D9";
+const colorNoDiv = "#B0C3D9";
 
-console.log("test");
+
+
+document.addEventListener("loaded", function(event){
+        colorScript();
+});
+
+
+function colorScript(){
+    var playerAdded;
+    playerAdded = document.getElementsByClassName("player  style-scope pugchamp-launchpad x-scope paper-icon-item-0");
+
+    for (var i = 0; i < playerAdded.length; i++) {
+        playerAdded[i].style.color = colorLow;
+    }
+
+    console.log(playerAdded);
+    console.log(playerAdded.length);
+};
