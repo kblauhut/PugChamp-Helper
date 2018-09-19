7023701eb4fd249401059898170378fce26f437e
unknown <kodeeey@gmail.com>
2018-09-19 20:44:55 +0200

    -MutationObserver now tracks players adding/removing

--- extension/src/contentscripts/eu/main.js
+++ extension/src/contentscripts/eu/main.js
@@ -9,16 +9,21 @@ chrome.storage.local.get(["colors", "elements"], function(result) {
     updateAll();
     let tables = document.getElementsByClassName(elements.table);
     for (var i = 0; i < tables.length; i++) {
-      addMutationObserver(tables[i])
+      addMutationObserver(tables[i], i)
     }
   });
 });
 
-function addMutationObserver(target) {
-  let config = { attributes: true, childList: true, characterData: true };
+function addMutationObserver(target, debugIndex) {
+  let config = {childList: true};
   let observer = new MutationObserver(function(mutations) {
     mutations.forEach(function(mutation) {
-      console.log(mutation.target);
+      if (mutation.removedNodes[0] != undefined && mutation.removedNodes[0].nodeName == "PAPER-ICON-ITEM") {
+        console.log("player removed @" + debugIndex);
+      }
+      if (mutation.addedNodes[1] != undefined && mutation.addedNodes[1].nodeName == "PAPER-ICON-ITEM") {
+        console.log("player added @" + debugIndex);
+      }
     });
   });
   observer.observe(target, config);
