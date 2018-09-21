f8a66199fbd9221a40b91bcf5c4e1830aa89bfbe
unknown <kodeeey@gmail.com>
2018-09-21 14:53:31 +0200

    -Added temp. solution for etf2l profile link

---
+++ extension/src/contentscripts/eu/profile.js
@@ -0,0 +1,20 @@
+document.addEventListener("loaded", function(event) {
+  let main = document.getElementById("mainContainer").children[0];
+  let buttonOuterHTML = document.createElement("a");
+  let buttonInnerHTML = main.getElementsByClassName("link x-scope paper-button-0")[1];
+  buttonInnerHTML = buttonInnerHTML.cloneNode(true);
+  let etf2lButton = main.appendChild(buttonOuterHTML);
+  buttonOuterHTML.className = "button";
+  buttonOuterHTML.setAttribute("href", getETF2LProfileUrl())
+  buttonInnerHTML.innerText = "ETF2L Profile";
+  buttonOuterHTML.appendChild(buttonInnerHTML);
+  buttonOuterHTML.style.position = "absolute";
+  buttonOuterHTML.style.left = 440+'px';
+  buttonOuterHTML.style.top = 144+'px';
+});
+
+function getETF2LProfileUrl() {
+  let etf2lURL = "http://etf2l.org/search/";
+  let id = document.URL.substring(document.URL.length -17, document.URL.length);
+  return etf2lURL + id;
+}
