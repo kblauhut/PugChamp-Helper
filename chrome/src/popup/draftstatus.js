7c48a35babfa18f59371deef59562b5bcc8917d9
sivbraec <simon.vanbraeckel@ugent.be>
2019-01-13 14:15:07 +0100

    Filter launchstatus updates + get added player list

--- chrome/src/popup/draftstatus.js
+++ chrome/src/popup/draftstatus.js
@@ -1,9 +1,5 @@
 var WebSocket = require('ws')
-
-
 var wsUri = "wss://eu.pug.champ.gg/socket.io/?EIO=3&transport=websocket";
-
-console.log("hi there love.");
 
 function testWebSocket() {
     websocket = new WebSocket(wsUri);
@@ -23,21 +19,32 @@ function testWebSocket() {
 
 function onOpen(evt) {
     console.log("Connection opened.");
-    doSend("WebSocket rocks");
+    
 }
 
 function onMessage(evt) {
     console.log("Message received.");
-    console.log(evt.data);
-    websocket.close();
+    let msg_text = evt.data;
+    if (isLaunchStatusUpdate(msg_text)){
+        msg_text = msg_text.replace(/^\d+/, '');    //remove leading numbers from json
+        console.log(msg_text);
+        let obj = JSON.parse(msg_text);             //convert to json
+        let class_player_dict = obj[1].playersAvailable;
+        console.log(class_player_dict);
+    }
+}
+
+function isLaunchStatusUpdate(msg_text){
+    return msg_text.includes("launchStatusUpdated");
 }
 
 function onError(evt) {
-    console.log("Error!");
+    console.log("An error occurred!");
+    //TODO do we need to close connection here? Check in API
 }
 
 function doSend(message) {
-    console.log("Sending message.");
+    console.log("Sending a message.");
     websocket.send(message);
 }
 
